25de5fe33d83c86b0c4cad116302b9ae9a81c76b
Documenter.jl <documenter@juliadocs.github.io>
2022-11-16 02:26:01 +0000

    build based on 04d1d78

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaOptimizationChallenge","category":"page"},{"location":"#JuliaOptimizationChallenge","page":"Home","title":"JuliaOptimizationChallenge","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaOptimizationChallenge.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaOptimizationChallenge]","category":"page"}]
+}
