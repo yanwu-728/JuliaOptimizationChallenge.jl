8cbbc65e0a9e3ea4fe03fa23cb4970c3d86c8a37
qhzhang <qhzhang95@gmail.com>
2022-11-21 00:21:25 -0500

    remove precompile

--- src/JuliaOptimizationChallenge.jl
+++ src/JuliaOptimizationChallenge.jl
@@ -7,7 +7,7 @@
 #    This may have bad interactions with precompilation.
 # 3) You are importing/using a package which has precompilation disabled
 
-__precompile__()
+#__precompile__()
 
 # If you are not going to precompile your package, it's good practice to disable it:
 # __precompile__(false)
@@ -32,7 +32,7 @@ module JuliaOptimizationChallenge
 # Note: You should try to keep exports to a minimum. Exports are the public API:
 # internal functionality should not be exported!
 
-export added_function, added_function2
+export added_function, added_function2, solution
 
 # Now include the real code
 # Write your code in other files, otherwise the package outline gets muddled
