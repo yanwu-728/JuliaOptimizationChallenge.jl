8cc0ec457df251c66d08e29d31c7367a80e5d5e1
yanwu-728 <74961465+yanwu-728@users.noreply.github.com>
2022-11-22 16:25:05 -0500

    Fix compilation error

--- src/upper_bound.jl
+++ src/upper_bound.jl
@@ -4,5 +4,5 @@ function compute_upper_bound()
     city = HashCode2014.read_city()
     (; total_duration, nb_cars, starting_junction, streets) = city
     itineraries = Vector{Vector{Int}}(undef, nb_cars)
-    
-return
+    return
+end
