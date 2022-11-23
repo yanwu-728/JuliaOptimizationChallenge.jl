09ac9ade16d2b0af6b824fbca0a70c5de9ac6a90
yanwu-728 <74961465+yanwu-728@users.noreply.github.com>
2022-11-22 20:21:16 -0500

    test change

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ using JuliaFormatter
     end
 
     # Skip formatting test for now
-    
+
     # @testset verbose = true "Code formatting (JuliaFormatter.jl)" begin
     #     @test format(JuliaOptimizationChallenge; verbose=true, overwrite=false)
     # end
@@ -21,16 +21,25 @@ using JuliaFormatter
     #     doctest(JuliaOptimizationChallenge)
     # end
 
+    @testset verbose = true "Small instance" begin
+        input_path = joinpath(@__DIR__, "data", "example_input.txt")
+        output_path = joinpath(@__DIR__, "data", "example_output.txt")
+        city = HashCode2014.read_city(input_path)
+        solution = JuliaOptimizationChallenge.solution(city)
+        @test HashCode2014.is_feasible(solution, city)
+        @test HashCode2014.total_distance(solution, city) == 450
+    end
+
     @testset verbose = true "Large instance" begin
         city = HashCode2014.read_city()
-        solution = solution(city)
+        solution = JuliaOptimizationChallenge.solution(city)
         @test city.total_duration == 54000
         @test HashCode2014.is_feasible(solution, city)
     end
 
     @testset verbose = true "Plotting" begin
         city = HashCode2014.read_city()
-        solution = solution(city)
+        solution = JuliaOptimizationChallenge.solution(city)
         HashCode2014.plot_streets(city, solution; path=nothing)
     end
 end
