0b324104b660b0f4d7ced084a6fd24bb8917c385
danvil <69361955+DVILL17@users.noreply.github.com>
2022-11-29 13:14:21 -0500

    Moved docstrings and tried removing dependency

--- src/upper_bound.jl
+++ src/upper_bound.jl
@@ -1,13 +1,13 @@
 using HashCode2014
 
-function compute_upper_bound()
-    """
+"""
     Calculate an upper bound on the number of meters that can be covered
 
     input: nothing
 
     output: nothing yet
-    """
+"""
+function compute_upper_bound()
     city = HashCode2014.read_city()
     (; total_duration, nb_cars, starting_junction, streets) = city
     itineraries = Vector{Vector{Int}}(undef, nb_cars)
