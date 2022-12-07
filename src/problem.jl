8ba441ab5f993fb0631fb122727de01dc78834a0
danvil <69361955+DVILL17@users.noreply.github.com>
2022-12-07 11:34:40 -0500

    cleaned up file

--- src/problem.jl
+++ src/problem.jl
@@ -5,7 +5,6 @@ Problem(city)
 
 """
 Store a problem which represents a city using a matrix and other parameters
-
 # Fields
     - total_duration::Int: total time allotted for the car itineraries (in seconds)
     - nb_cars::Int: number of cars in the fleet
@@ -27,29 +26,22 @@ Creates a Problem instance using a HashCode2014.jl City object
 function Problem(city::City)
     J = length(city.junctions)
     A = Vector{Vector{Street}}(undef,J)
-
+    for i =1:length(A)
+        A[i] = []
+    end
     for street in city.streets
         (;endpointA,endpointB,bidirectional,duration,distance) = street
-        if !isdefined(A,endpointA)
-            A[endpointA] = [street]
-        else
-            A[endpointA].append!([street])
-        end
+        push!(A[endpointA],street)
         if bidirectional
-            if !isdefined(A,endpointB)
-                A[endpointB] = [street]
-            else
-                A[endpointB].append!([street])
-            end
+            push!(A[endpointB],street)
         end
     end
-
-    problem = Problem(;
-        total_duration=city.total_duration,
-        nb_cars=city.nb_cars,
-        starting_junction=city.starting_junction,
-        junctions=city.junctions,
-        adjacency=A
-    )
+    
+    problem = Problem(;total_duration=city.total_duration,
+                    nb_cars=city.nb_cars,
+                    starting_junction=city.starting_junction,
+                    junctions=city.junctions,
+                    adjacency=A
+                    )
     return problem
 end
