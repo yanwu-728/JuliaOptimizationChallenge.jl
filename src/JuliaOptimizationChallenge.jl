9db79fb99f3b43c0d2742f700b5dec4ca947a149
danvil <69361955+DVILL17@users.noreply.github.com>
2022-12-06 22:40:15 -0500

    made some tweaks

--- src/JuliaOptimizationChallenge.jl
+++ src/JuliaOptimizationChallenge.jl
@@ -35,7 +35,7 @@ module JuliaOptimizationChallenge
 using HashCode2014
 
 export solver
-export problem
+#export problem
 
 # Now include the real code
 # Write your code in other files, otherwise the package outline gets muddled
--- src/upper_bound.jl
+++ src/upper_bound.jl
@@ -6,6 +6,8 @@ compute_upper_bound(city)
 Calculate an upper bound on the number of meters that can be covered using a HashCode2014.jl City instance
 """
 function compute_upper_bound(city)
+    #Make an upper bound that depends on the parameters of the problem like time allowed, number of cars, ...
+    #Can be anything as long as you justify a proof for this that you include in documentation
     bound = 0
     (; total_duration, nb_cars, starting_junction, streets) = city
     for street in streets
