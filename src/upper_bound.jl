793a15789825da12811fe14d07466969045a39ec
danvil <69361955+DVILL17@users.noreply.github.com>
2022-12-09 18:11:59 -0500

    slight change to upper bound

--- src/upper_bound.jl
+++ src/upper_bound.jl
@@ -8,8 +8,8 @@ compute_upper_bound(city)
 Calculate an upper bound on the number of meters that can be covered using a HashCode2014.jl City instance
 """
 function compute_upper_bound(city)
-    upper_bound = 0
-    time = 0
+    upper_bound = 0.0
+    time = 0.0
     (; total_duration, nb_cars, starting_junction, streets) = city
     sort!(streets; by=street -> street.distance / street.duration, rev=true)
     for street in streets
